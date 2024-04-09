3aad28b1baa9b5027e9714fdd464efbb50fb950e
Kuly14 <kulman.david@gmail.com>
2024-04-09 16:06:13 +0200

    Fix revm-test tests

--- bins/revme/src/cmd/statetest/utils.rs
+++ bins/revme/src/cmd/statetest/utils.rs
@@ -1,11 +1,11 @@
-use k256::ecdsa::SigningKey;
+use libgoldilocks::SigningKey;
 use revm::primitives::Address;
 
 /// Recover the address from a private key (SigningKey).
 pub fn recover_address(private_key: &[u8]) -> Option<Address> {
-    let key = SigningKey::from_slice(private_key).ok()?;
-    let public_key = key.verifying_key().to_encoded_point(false);
-    Some(Address::from_raw_public_key(&public_key.as_bytes()[1..]))
+    let key = SigningKey::from_slice(private_key);
+    let public_key = key.verifying_key();
+    Some(Address::from_raw_public_key(&public_key.as_bytes()))
 }
 
 #[cfg(test)]
@@ -16,9 +16,9 @@ mod tests {
     #[test]
     fn sanity_test() {
         assert_eq!(
-            Some(address!("a94f5374fce5edbc8e2a8697c15331677e6ebf0b")),
+            Some(address!("1afe4bd57060cb20be3da71729151922bcbf3947")),
             recover_address(&hex!(
-                "45a915e4d060149eb4365960e6a7a45f334393093061116b197e3240065ff2d8"
+                "445a915e4d060149eb4365960e6a7a45f334393093061116b197e3240065ff2d85a915e4d060149eb4365960e6a7a45f334393093061110068"
             ))
         )
     }
